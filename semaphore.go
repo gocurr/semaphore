3e356f313dcb9d9e91eb0b4754e724ac384b9eb3
jes <sample@gmail.com>
2021-09-16 22:07:16 +0800

    try to acquire; timeout acquire

--- semaphore.go
+++ semaphore.go
@@ -1,6 +1,9 @@
 package gosem
 
-import "errors"
+import (
+	"errors"
+	"time"
+)
 
 type SemPool chan *Semaphore
 
@@ -8,12 +11,12 @@ type Semaphore struct {
 	pool *SemPool
 }
 
-func New(size int) SemPool {
-	if size <= 0 {
-		panic(errors.New("negative or 0 cannot be a size"))
+func New(permits int) SemPool {
+	if permits <= 0 {
+		panic(errors.New("negative or 0 cannot be a permits"))
 	}
-	pool := make(chan *Semaphore, size)
-	for i := 0; i < size; i++ {
+	pool := make(chan *Semaphore, permits)
+	for i := 0; i < permits; i++ {
 		pool <- &Semaphore{pool: (*SemPool)(&pool)}
 	}
 	return pool
@@ -21,6 +24,30 @@ func New(size int) SemPool {
 
 func (s SemPool) Acquire() *Semaphore {
 	return <-s
+}
+
+func (s SemPool) TryAcquire() (*Semaphore, error) {
+	select {
+	case ret := <-s:
+		return ret, nil
+	default:
+		return nil, errors.New("no permits available")
+	}
+}
+
+func (s SemPool) TryAcquireTimeout(timeout time.Duration) (*Semaphore, error) {
+	for {
+		select {
+		case ret := <-s:
+			return ret, nil
+		case <-time.After(timeout):
+			return nil, errors.New("timeout")
+		}
+	}
+}
+
+func (s SemPool) Available() int {
+	return len(s)
 }
 
 func (s *Semaphore) Release() {
--- semaphore_test.go
+++ semaphore_test.go
@@ -31,3 +31,34 @@ func Test_Sem(t *testing.T) {
 
 	wg.Wait()
 }
+
+func Test_Sem_TryAcquire(t *testing.T) {
+	rand.Seed(time.Now().Unix())
+
+	semPool := New(runtime.NumCPU())
+	fmt.Printf("begin: %d\n", semPool.Available())
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+
+		go func(i int) {
+			semaphore, err := semPool.TryAcquireTimeout(2 * time.Second)
+			if err != nil {
+				available := semPool.Available()
+				fmt.Printf("semaphore is nil, available %d\n", available)
+				wg.Done()
+				return
+			}
+			// mock slow operation
+			n := rand.Intn(5)
+			time.Sleep(time.Duration(n) * time.Second)
+			fmt.Printf("%d released\n", i)
+			semaphore.Release()
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+}
